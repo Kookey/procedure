49efee8c04bbc6bdce6d287f540ae689a22b250e
Kookey <wxlhdm@qq.com>
2013-08-26 11:37:37 +0800

    procedure的java调用

---
+++ src/com/steel/entry/User.java
@@ -0,0 +1,35 @@
+package com.steel.entry;
+
+public class User {
+
+	private String id;
+	private String name;
+	private String pwd;
+	private String address;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
---
+++ src/com/steel/util/DBConnection.java
@@ -0,0 +1,63 @@
+package com.steel.util;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DBConnection {
+	
+	private static Properties prop = null;
+	static {
+		prop = new Properties();
+		try {
+			prop.load(DBConnection.class.getClassLoader().getResourceAsStream("db.properties"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static Connection getConnection(){
+		Connection conn = null;
+		String driver = prop.getProperty("driver");
+		String url = prop.getProperty("url");
+		String username = prop.getProperty("username");
+		String pwd = prop.getProperty("password");
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, username, pwd);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+	public static void close(ResultSet rs,Statement st,Connection conn){
+		if(rs!=null){
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if(st!=null){
+			try {
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if(conn != null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
